efb7d1c6e321b332c9a84b673cab1329da5f707f
maruf786 <maruf@modello.my>
2021-03-21 12:01:28 +0600

    Pages router add

---
+++ pages/index.jsx
@@ -0,0 +1,15 @@
+import react from 'react';
+import ReactDom from 'react-dom';
+
+const data =[
+    
+]
+const App = ()=>{
+    return (
+        <div>
+            Hi this is a next.js project
+        </div>
+    )
+}
+
+export default App;

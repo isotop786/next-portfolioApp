950af273847b584e8cb43f8d6a84464d234a1a3d
maruf786 <maruf@modello.my>
2021-03-27 02:07:59 +0600

    portfolio update

--- pages/portfolio.js
+++ pages/portfolio.js
@@ -10,14 +10,31 @@ const Portfolio = ()=>{
             <div className="row mt-4">
                 <div className="col-sm-4 p-3">
                     <div className="card card-default ">
-                    <h3 className="card-header">Hacker News Clone</h3>
+                    <h3 className="card-header">TODO APP </h3>
                     <div className="card-body">
                         <p>
-                            Hacker News site clone using API and next.js
+                            A todo application using Node and Express.js 
+                        </p><p>
+                            Login : maruf
+                            Password: maruf
                         </p>
                         <div className="d-flex justify-content-between">
-                            <a className="btn btn-info" href="#">Visit the site</a>
+                            <a className="btn btn-info" href="https://my-todo-app-778866.herokuapp.com/">Visit the site</a>
                             <a className="btn btn-danger " href="#">Code</a>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+                <div className="col-sm-4 p-3">
+                    <div className="card card-default ">
+                    <h3 className="card-header">Movie Trailer App </h3>
+                    <div className="card-body">
+                        <p>
+                            A movie trailler app using React / Next.js and Node.js as backend
+                        </p>
+                        <div className="d-flex justify-content-between">
+                            <a className="btn btn-info" href="https://maruf-movieapp.herokuapp.com/">Visit the site</a>
+                            <a className="btn btn-danger " href="https://github.com/isotop786/movieapp-react">Code</a>
                         </div>
                     </div>
                     </div>
